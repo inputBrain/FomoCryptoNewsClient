89c527e01103ac0076e83a368ac383f7edce82a0
Alexander <vsany@meta.ua>
2024-07-02 09:01:18 +0300

    + news model

---
+++ FomoCryptoNews.Client/Models/NewsModel.cs
@@ -0,0 +1,30 @@
+﻿using Bogus;
+
+namespace FomoCryptoNews.Client.Models;
+
+public class NewsModel
+{
+    public Guid  Id { get; set; }
+    
+    public string Title { get; set; }
+    
+    public string Description { get; set; }
+    
+    public string Cover { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    
+    
+    public static List<NewsModel> FakerCollection()
+    {
+        var faker = new Faker<NewsModel>()
+            .RuleFor(n => n.Id, f => Guid.NewGuid())
+            .RuleFor(n => n.Title, f => f.Lorem.Sentence(5, 7))
+            .RuleFor(n => n.Description, f => f.Lorem.Paragraphs(1, 3)) 
+            .RuleFor(n => n.Cover, f => f.Image.PicsumUrl()) 
+            .RuleFor(n => n.CreatedAt, f => f.Date.Between(DateTime.Now.AddMonths(-1), DateTime.Now));
+
+        var collection = faker.Generate(10);
+        return collection;
+    }
+}
