8cc700c3aa52986ae2e26a22037435774a6b304f
Alexander <vsany@meta.ua>
2024-07-06 03:00:14 +0300

    + table. * small fix components

--- FomoCryptoNews.Client/Program.cs
+++ FomoCryptoNews.Client/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+var mvcBuilder = builder.Services.AddControllersWithViews();
+mvcBuilder.AddRazorRuntimeCompilation();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
